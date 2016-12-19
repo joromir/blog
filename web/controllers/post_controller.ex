b055fdbf2312bb4f2213a0a20e0d947e0345e6c9
Georgi N. Kostov <gnkostov@yahoo.com>
2016-12-20 01:48:16 +0200

    generate Post model

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -4,4 +4,9 @@ defmodule Blog.PostController do
   def index(conn, _) do
     render conn, "index.html"
   end
+
+  def new(conn, _params) do
+    changeset = Post.changeset(%Post{})
+    render(conn, "new.html", changeset: changeset)
+  end
 end
---
+++ web/models/post.ex
@@ -0,0 +1,20 @@
+defmodule Blog.Post do
+  use Blog.Web, :model
+
+  schema "posts" do
+    field :title, :string
+    field :body, :string
+    field :user_id, :integer
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :body, :user_id])
+    |> validate_required([:title, :body, :user_id])
+  end
+end
