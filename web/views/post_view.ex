ca92667855e663861305f62ec5f6da703e434a5a
Georgi N. Kostov <gnkostov@yahoo.com>
2016-12-29 11:37:22 +0200

    refactor view code

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -5,6 +5,10 @@ defmodule Blog.PostView do
     link post.title, to: post_path(conn, :show, post)
   end
 
+  def new_post_button(conn) do
+    link "New Post", to: post_path(conn, :new), class: 'btn btn-success btn-lg'
+  end
+
   def no_comments_notification(conn, comments) do
     if Enum.count(comments) == 0, do: "Not a single comment is given for now.."
   end
