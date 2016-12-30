68c726ab67725b459ddef9b04c5019449580044f
Georgi N. Kostov <gnkostov@yahoo.com>
2016-12-31 00:30:20 +0200

    refactor CommentController create action and define method create_comment in Post model

--- web/controllers/comment_controller.ex
+++ web/controllers/comment_controller.ex
@@ -1,19 +1,17 @@
 defmodule Blog.CommentController do
   use Blog.Web, :controller
 
-  alias Blog.Comment
   alias Blog.Repo
   alias Blog.Post
 
   def create(conn, %{"comment" => comment_params, "post_id" => post_id}) do
-    changeset = %Comment{}
-    |> Comment.changeset(Map.merge(comment_params, %{"post_id" => post_id}))
+    post = Repo.get!(Post, post_id)
 
-    case Repo.insert(changeset) do
+    case Post.create_comment(post, comment_params) do
       {:ok, comment} ->
         conn
         |> put_flash(:info, "Saved! #{}")
-        |> redirect(to: post_path(conn, :show, Repo.get!(Post, post_id)))
+        |> redirect(to: post_path(conn, :show, post))
       _ -> 
         conn
         |> text("Something really bad happened!!!")
--- web/models/post.ex
+++ web/models/post.ex
@@ -1,6 +1,9 @@
 defmodule Blog.Post do
   use Blog.Web, :model
 
+  alias Blog.Comment
+  alias Blog.Repo
+
   schema "posts" do
     field :title, :string
     field :body, :string
@@ -16,5 +19,12 @@ defmodule Blog.Post do
     struct
     |> cast(params, [:title, :body, :user_id, :short_description])
     # |> validate_required([:title, :body, :user_id])
+  end
+
+  def create_comment(struct, comment_params) do
+    new_comment = %Comment{}
+    |> Comment.changeset(Map.merge(comment_params, %{"post_id" => struct.id}))
+
+    if Repo.insert(new_comment), do: {:ok, new_comment}, else: {:error, "Error"}
   end
 end
